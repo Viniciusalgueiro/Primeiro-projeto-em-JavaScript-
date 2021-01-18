9f4337c205e87e694cd2182b1cac5d63a767180a
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-01-17 23:57:31 -0300

    keep

--- Poo_studing/index.js
+++ Poo_studing/index.js
@@ -5,4 +5,14 @@ class Cliente {
     saldo;
 }
 
+const cliente1 = new Cliente(); 
+cliente1.Nome = "Ricardo";
+cliente1.CPF = 11122233309;
+cliente1.agencia = 1001;
+cliente1.Saldo = 0;
+
+
+
+console.log(cliente1Nome);
+
 
