46e15ad7011617b6d2f4d1cbde5d516b7582613d
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-01-18 00:26:06 -0300

    inicial

--- Poo_studing/index.js
+++ Poo_studing/index.js
@@ -1,18 +1,41 @@
 class Cliente {
-    Nome;
+    nome;
     cpf;
+    
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente(); 
-cliente1.Nome = "Ricardo";
-cliente1.CPF = 11122233309;
-cliente1.agencia = 1001;
-cliente1.Saldo = 0;
+const cliente2 = new Cliente();
 
 
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
 
-console.log(cliente1Nome);
+
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.saldo = 1000;
+ContaCorrenteRicardo.agencia = 1001;
+
+
+ContaCorrenteRicardo.sacar(10000);
+console.log(ContaCorrenteRicardo.saldo);
+
+
+console.log(cliente1);
+console.log(cliente2);
 
 
