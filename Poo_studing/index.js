93683fb7421cd31d9bac779f7bafc4a7c297fcaf
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-01-18 16:10:47 -0300

    vamo que vamo

--- Poo_studing/index.js
+++ Poo_studing/index.js
@@ -6,36 +6,39 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo=0;
     sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
         }
     }
+    
+    depositar(valor){
+        if(valor <= 0 ){
+            return;
+        }
+            this._saldo += valor;
+        }
 }
 
 const cliente1 = new Cliente(); 
-const cliente2 = new Cliente();
-
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 
 const ContaCorrenteRicardo = new ContaCorrente();
-ContaCorrenteRicardo.saldo = 1000;
 ContaCorrenteRicardo.agencia = 1001;
+ContaCorrenteRicardo.depositar(1000);
 
+const valorSacado = ContaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
 
-ContaCorrenteRicardo.sacar(10000);
-console.log(ContaCorrenteRicardo.saldo);
+console.log(ContaCorrenteRicardo);
 
-
-console.log(cliente1);
-console.log(cliente2);
 
 
