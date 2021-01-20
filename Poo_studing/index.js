44b16b953036925f901a2b6f4f1b7f8062615642
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-01-19 23:36:51 -0300

    atualizando e editando

--- Poo_studing/index.js
+++ Poo_studing/index.js
@@ -1,35 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-    
-}
-
-class ContaCorrente {
-    agencia;
-    cliente;
-
-
-    _saldo=0;
-
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-    
-    depositar(valor){
-        if(valor <= 0 ){
-            return;
-        }
-            this._saldo += valor;
-        }
-    transferir(valor , conta) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-    }   
-}
+import {cliente} from "./client.js"
+import {ContaCorrente} from "./ContaCorrente"
 
 const cliente1 = new Cliente(); 
 cliente1.nome = "Ricardo";
@@ -39,14 +9,14 @@ const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
-
 const ContaCorrenteRicardo = new ContaCorrente();
 ContaCorrenteRicardo.agencia = 1001;
 ContaCorrenteRicardo.cliente = cliente1
+ContaCorrenteRicardo.depositar(500);
+
 const conta2 = new ContaCorrente();
 conta2.cliente = cliente2
 conta2.agencia = 102;
-ContaCorrenteRicardo.depositar(500);
 
 
 console.log (conta2);
